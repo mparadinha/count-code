33dd5dda8a11acf870c2422da6dbbbfa71d2b1a2
Miguel Paradinha <miguel.p.paradinha@gmail.com>
2016-06-04 13:29:36 +0100

    can now order files by number of lines

--- count_code.py
+++ count_code.py
@@ -4,31 +4,31 @@ from argparse import ArgumentParser
 
 def get_filepaths(path):
     files = []
-    
+
     for f in [os.path.join(path, i) for i in os.listdir(path)]:
         ext = f.split(".")[-1]
         if ext == "py": files.append(f)
-        
+
         if os.path.isdir(f):
             files.extend(get_filepaths(os.path.join(path, f)))
-      
+
     return files
 
 def lines_of_code(filename):
     data = None
     with open(filename, "r") as f: data = f.read()
-    
+
     # cut multiline comments
     data = "".join(data.split("\"\"\"")[::2])
-    
+
     lines = data.split("\n")
-    
+
     # cut one line comments
     lines = [line for line in lines if not line.startswith("#")]
-    
+
     # remove empty lines
     lines = [line for line in lines if line.strip() != ""]
-    
+
     return len(lines)
 
 def main():
@@ -36,28 +36,40 @@ def main():
     parser.add_argument("-p", "--path", action="append", dest="paths",
                         help="specify subdirectories to scan, defaults to current directory")
     parser.add_argument("-v", "--verbose", action="store_true",
-                        help="show more stuff on screen")
+                        help="show more information on screen")
+    parser.add_argument("-o", "--ordered", action="store_true",
+                        help="show files ordered number of lines (crescent)")
     args = parser.parse_args()
     if not args.paths:
         args.paths = [os.getcwd()]
-    
+
+    # get all files to count
     files = []
     for path in args.paths:
         start_dir = os.getcwd()
         os.chdir("".join(["." for _ in range(path.count("."))]))
-        
+
         files.extend(get_filepaths(os.path.join(os.getcwd(), path.replace(".", ""))))
-        
+
         os.chdir(start_dir)
-        
+
+    # count number of lines
     lines = list(map(lines_of_code, files))
-    
+
     if args.verbose:
-        d = dict(zip(files, lines))
-        for k in sorted(d.keys()):
-            print("/".join(k.split("\\")[-2:]), "-->", d[k], "lines") 
-                  
+        pretty = dict(zip(files, lines))
+        if args.ordered:
+            # sort by number of lines
+            keys = sorted(pretty.keys(), key=lambda k: pretty[k], reverse=True)
+        else:
+            keys = sorted(pretty.keys())
+
+        # print info
+        for k in keys:
+            filename = "/".join(k.split("\\")[-2:])
+            print(filename, "-->", pretty[k], "line" if pretty[k] == 1 else "lines")
+
     print(sum(lines), "lines of code in total")
-    
+
 if __name__ == "__main__":
     main()
